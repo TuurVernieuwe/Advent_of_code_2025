a8b22b26b1df2266aba9c69cbfa8f24f5239e4a5
Tuur Vernieuwe <vernieuwe.tuur@gmail.com>
2025-12-04 13:26:30 +0100

    day 4 part 1

---
+++ Day4/Part1/solution_part1.py
@@ -0,0 +1,25 @@
+with open('C:/Users/tuur1/OneDrive/Bureaublad/Advent of code 2025/Advent_of_code_2025/Day4/Inputs/input.txt', 'r') as file:
+    input_data = file.read().strip().split('\n')
+
+input_matrix = []
+for input in input_data:
+    input_matrix.append(list(input))
+
+number_of_rows = len(input_matrix)
+number_of_columns = len(input_matrix[0])
+directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+res = 0
+for row in range(number_of_rows):
+    for column in range(number_of_columns):
+        if input_matrix[row][column] == '@':
+            number_of_neigbours = 0
+            for direction in directions:
+                if (((row + direction[0]) >= 0) and ((row + direction[0]) < number_of_rows)):
+                    if (((column + direction[1]) >= 0) and ((column + direction[1] < number_of_columns))):
+                        if input_matrix[row + direction[0]][column + direction[1]] == '@':
+                            number_of_neigbours += 1
+            if number_of_neigbours < 4:
+                res += 1
+
+print(res)
